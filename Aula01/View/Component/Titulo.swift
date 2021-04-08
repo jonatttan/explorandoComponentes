785bd616d2310427058bb168f4b4d2fa5c55e88d
Jonattan <jonattan.sousa@gmail.com>
2021-04-08 17:12:42 -0300

    Foi implementado a atualização da atividade, porém não percebido melhora no resultado, aparenta ser mais complexo e não agregou algo novo

--- Aula01/View/Component/Titulo.swift
+++ Aula01/View/Component/Titulo.swift
@@ -15,38 +15,32 @@ protocol TituloDelegate: class {
 public class Titulo: UIView {
     weak var delegate: TituloDelegate?
     var buttonAction: (() -> Void)?
-    
     let nibName = "Vamos mudar."
-    var contentView:UIView?
+    // var contentView:UIView?
     var tituloHere = ""
 
     struct EnumTitulo {
         let name: String
         let background: UIColor
     }
-    
     @IBOutlet private var labelTitulo: UILabel?
-    
     required init?(coder aDecoder: NSCoder) {
         super.init(coder: aDecoder)
         setupInit()
     }
-    
     init(title: String) {
         super.init(frame: CGRect(x: 100, y: 100, width: 100, height: 100))
         self.tituloHere = "CoisaQualquer"
         layoutIfNeeded()
     }
-    
     override public func awakeFromNib() {
         super.awakeFromNib()
+        setupUICell()
     }
-    
     override public func layoutSubviews() {
         super.layoutSubviews()
         setupUIView()
     }
-    
     public func addTitulo(titulo: String, positionY: Int, cor: UIColor) {
         let titleLabel = UILabel(frame: CGRect(x: 0, y: 0, width: 200, height: 20))
         titleLabel.text = titulo
@@ -56,28 +50,23 @@ public class Titulo: UIView {
         self.addSubview(titleLabel)
         self.translatesAutoresizingMaskIntoConstraints = false
     }
-    
     private func setupInit() {
         if self.tituloHere.isEmpty {
             self.tituloHere = "1o Preenchimento"
         }
     }
-    
-    func setupUI(title: String) {
+    public func setupUI(title: String) {
         DispatchQueue.main.async {
             self.tituloHere = title
             self.labelTitulo?.text = title
         }
     }
-    
     private func setupUICell() {
         self.labelTitulo?.text = tituloHere
     }
-    
     private func setupUIView() {
         self.labelTitulo?.text = tituloHere
     }
-    
     @IBAction func btAction(_ sender: UIButton) {
         if let kButonAction = buttonAction {
             kButonAction()
@@ -85,5 +74,4 @@ public class Titulo: UIView {
             delegate?.buttonAction()
         }
     }
-    
 }
